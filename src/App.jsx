7b165f3433e1b626dee9747adbf8f684f99795c5
Khandaker Sajjad Hossen <sk.sajjad4207@gmail.com>
2023-10-26 14:45:54 +0600

    navbar added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import "./App.css";
+import Navbar from "./shared/Navbar/Navbar";
 
 function App() {
   
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Navbar/>
     </>
   );
 }
---
+++ src/shared/Navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <header>
+            <div className="fullNavbar bg-black flex justify-between p-3">
+                <div className="logo"><h1 className='text-white text-2xl font-mono'>Heels villa</h1></div>
+                <nav>.</nav>
+            </div>
+        </header>
+    );
+};
+
+export default Navbar;
