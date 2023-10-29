0497b83cf220e97297ac33a957d77b00ce1cd987
Khandaker Sajjad Hossen <sk.sajjad4207@gmail.com>
2023-10-29 15:45:34 +0600

    navbar sucessfully set

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -15,6 +15,9 @@ const Products = () => {
     setItems(newCart)
   };
 
+
+
+
   return (
     <div className="container mx-auto">
       <h1 className="text-center my-3 font-semibold text-2xl font-mono">
@@ -34,15 +37,15 @@ const Products = () => {
           <div className="orderSummery p-3">
             <div className="orderText flex justify-between">
               <h1 className="text-xl font-mono">Order summery</h1>
-              <button className="bg-black rounded-sm text-white p-1">
-                <AiFillDelete />
+              <button  className="bg-black rounded-sm text-white p-1">
+                < AiFillDelete />
               </button>
             </div>
             <div className="singleCart mt-10">
                 {
                     items.map(item => <div 
                     key={item.id}
-                    className="border-2 border-black flex justify-between px-3"
+                    className="border-2 border-black flex justify-between px-3 mb-2"
                     >
                         <img className="w-1/3" src={item.sideImage} alt="" />
                         <div className="texts text-xs my-auto ">
--- src/shared/Navbar/Navbar.jsx
+++ src/shared/Navbar/Navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Navbar = () => {
     return (
-        <header>
+        <header className='sticky top-0 z-20'>
             <div className="fullNavbar bg-black flex justify-between p-3 sticky top-0">
                 <div className="logo"><h1 className='text-white text-2xl font-mono'>Heels villa</h1></div>
                 <nav>.</nav>
